8688a2b46a5f463c6d60770c808f7dd6edab1853
Nerea Battauz <nereebattauz@gmail.com>
2024-04-13 21:26:24 -0300

    menu css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'teal': '#2dd4bf',
+      'light-teal': '#99f6e4',
+      'dark-teal': '#14b8a6',
+      'light-grey': '#f1f5f9',
+      'black': '#334155',
+      'green': '#22c55e',
+      'red': '#e11d48',
+      'blue': '#2563eb',
+    }
+    },
   },
   plugins: [],
 }
