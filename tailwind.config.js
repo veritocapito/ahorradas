51f7a18aaa125894f993d0837ceadd1b1b777acd
Nerea Battauz <nereebattauz@gmail.com>
2024-04-21 20:27:42 -0300

    estilos cards mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      'teal': '#2dd4bf',
-      'light-teal': '#99f6e4',
-      'dark-teal': '#14b8a6',
-      'light-grey': '#f1f5f9',
-      'black': '#334155',
-      'green': '#22c55e',
-      'red': '#e11d48',
-      'blue': '#2563eb',
-    }
+        teal: "#2dd4bf",
+        "light-teal": "#99f6e4",
+        "dark-teal": "#14b8a6",
+        "light-grey": "#f1f5f9",
+        black: "#3f3f46",
+        green: "#22c55e",
+        red: "#e11d48",
+        blue: "#2563eb",
+        bgwhite: "#f6f6f6",
+      },
+      backgroundImage: {
+        ahorrar: "url('/assets/img-ahorrar.png')",
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
