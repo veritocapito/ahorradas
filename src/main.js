019fa870e278b527fd6fbb2f6df04461fd3d2a5b
Veronica Solange Capobianco <veritocapito@gmail.com>
2024-04-25 18:38:38 -0300

    add vistas balance-categorias-reportes

---
+++ src/main.js
@@ -0,0 +1,39 @@
+const btnBalance = document.getElementById('btn-balance');
+const btnCategorias = document.getElementById('btn-categorias');
+const btnReportes = document.getElementById('btn-reportes');
+const btnNuevaOperacion = document.getElementById('btn-nueva-operacion');
+const btnCancelarOperacion = document.getElementById('btn-cancelar-operacion');
+const btnAgregarOperacion = document.getElementById('btn-agregar-operacion');
+
+const vistaBalance = document.getElementById('vista-balance');
+const vistaCategorias = document.getElementById('vista-categorias');
+const vistaReportes = document.getElementById('vista-reportes');
+const vistaNuevaOperacion = document.getElementById('vista-nueva-operacion');
+
+const cambiarVista = (mostrarVista) => {
+    vistaBalance.classList.add('hidden');
+    vistaCategorias.classList.add('hidden');
+    vistaReportes.classList.add('hidden');
+    vistaNuevaOperacion.classList.add('hidden');
+
+    mostrarVista.classList.remove('hidden');
+}
+
+btnBalance.addEventListener('click', () => cambiarVista(vistaBalance));
+btnCategorias.addEventListener('click', () => cambiarVista(vistaCategorias));
+btnReportes.addEventListener('click', () => cambiarVista(vistaReportes));
+btnNuevaOperacion.addEventListener('click', () => cambiarVista(vistaNuevaOperacion));
+btnCancelarOperacion.addEventListener('click', () => cambiarVista(vistaBalance));
+
+
+const btnFiltros = document.getElementById('btn-filtros');
+const vistaFiltros = document.getElementById('vista-filtros');
+
+const ocultarFiltros = () => {
+    vistaFiltros.classList.toggle('hidden');
+    btnFiltros.innerHTML === 'Ocultar Filtros' 
+    ?   btnFiltros.innerHTML = 'Mostrar Filtros' 
+    :   btnFiltros.innerHTML = 'Ocultar Filtros'
+}
+
+btnFiltros.addEventListener('click', ocultarFiltros);
