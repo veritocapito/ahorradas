78992b288bd20c0d99f34d2908a1b07959242732
Veronica Solange Capobianco <veritocapito@gmail.com>
2024-05-21 22:08:51 -0300

    add funcionalidad nueva operacion

--- src/main.js
+++ src/main.js
@@ -1,3 +1,47 @@
+//cargar categorias
+const categoriasSelect = document.querySelector('#categorias-select');
+
+function cargarCategorias(categorias){
+    categorias.forEach(categoria => {
+        let nuevaCategoria = document.createElement('option');
+        nuevaCategoria.value = categoria
+        nuevaCategoria.textContent = categoria
+        categoriasSelect.appendChild(nuevaCategoria)
+    });
+}
+
+//cargar storage
+
+function cargarStorage() {
+    const categorias = localStorage.getItem('categorias')
+    const operaciones = localStorage.getItem('operaciones')
+
+    if(!categorias){
+        const categoriasDefault = ["Comida", "Servicios", "Salidas", "Educación","Transporte","Trabajo"  ]
+        localStorage.setItem('categorias', categoriasDefault)
+        cargarCategorias(categoriasDefault)
+    } else{
+        let nuevaCategoria = ''
+        let nuevasCategoriasArray = []
+
+        for (let i=0; i< categorias.length; i++) {
+            if (categorias[i] !== ",") {
+                nuevaCategoria += categorias[i]
+                if(i === categorias.length -1){
+                    nuevasCategoriasArray.push(nuevaCategoria)
+                }
+            } else {
+                nuevasCategoriasArray.push(nuevaCategoria)
+                nuevaCategoria = ''
+            }
+        }
+        cargarCategorias(nuevasCategoriasArray)
+    }
+}
+
+cargarStorage()
+
+//Cambiar Vistas
 const btnBalance = document.getElementById('btn-balance');
 const btnCategorias = document.getElementById('btn-categorias');
 const btnReportes = document.getElementById('btn-reportes');
@@ -34,6 +78,7 @@ btnNuevaOperacion.addEventListener('click', () => cambiarVista(vistaNuevaOperaci
 btnCancelarOperacion.addEventListener('click', () => cambiarVistaBalance(vistaBalance));
 
 
+// Boton Filtros
 const btnFiltros = document.getElementById('btn-filtros');
 const vistaFiltros = document.getElementById('vista-filtros');
 
@@ -46,6 +91,8 @@ const ocultarFiltros = () => {
 
 btnFiltros.addEventListener('click', ocultarFiltros);
 
+
+// Menu Hamburguesa
 const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
 
@@ -63,3 +110,46 @@ btnMenu.addEventListener('click', () => {
         menu.classList.add('top-[80px]', 'opacity-100');
     }
 });
+
+
+//Nueva Operacion
+
+const descripcionNuevaOperacion = document.getElementById('descripcion-nueva-operacion');
+const montoNuevaOperacion = document.getElementById('monto-nueva-operacion');
+const tipoNuevaOperacion = document.getElementById('tipo-nueva-operacion');
+const categoriaNuevaOperacion = document.getElementById('categoria-nueva-operacion');
+const fechaNuevaOperacion = document.getElementById('fecha-nueva-operacion');
+
+function crearOperacion() {
+
+    let nuevaOperacion = {
+        descripcion: descripcionNuevaOperacion.value,
+        monto: montoNuevaOperacion.value,
+        tipo: tipoNuevaOperacion.value,
+        categoria: categoriaNuevaOperacion.value,
+        fecha: fechaNuevaOperacion.value
+    }
+
+    console.log(nuevaOperacion)
+    const operaciones = localStorage.getItem("operaciones")
+    console.log(operaciones)
+    if (operaciones === null) {
+        console.log("Operaciones es null")
+        let nuevoArray = [{ nuevaOperacion }]
+        localStorage.setItem("operaciones", JSON.stringify(nuevoArray))
+    } else {
+        console.log("Operaciones no es null")
+        let parsedStorage = JSON.parse(localStorage.getItem("operaciones"))
+        parsedStorage.push(nuevaOperacion)
+        localStorage.setItem("operaciones", JSON.stringify(parsedStorage))
+    }
+    console.log("Operacion Creada")
+}
+
+btnAgregarOperacion.addEventListener('click', function (event) {
+        event.stopPropagation()
+        event.preventDefault()
+        event.stopImmediatePropagation()
+        crearOperacion()
+    }
+);
